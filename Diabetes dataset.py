de29f3b954b4b7ced146bb4b0a489743d1de3814
Dilacerare <aidar.galeeff@gmail.com>
2023-06-02 16:53:42 +0500

    01.06.2023

--- Diabetes dataset.py
+++ Diabetes dataset.py
@@ -1,7 +1,10 @@
+import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_diabetes
 import pandas as pd
 import seaborn as sns
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import r2_score
 
 # Загрузка датасета Diabetes
 diabetes = load_diabetes()
@@ -12,31 +15,38 @@ df = pd.DataFrame(diabetes.data, columns=diabetes.feature_names)
 # Добавление столбца с целевой переменной
 df['target'] = diabetes.target
 
-myBMI = 70/(1.85**2)
-print(f"myBMI = {myBMI}")
+###########################################################################
+
+# myBMI = 70/(1.85**2)
+# print(f"myBMI = {myBMI}")
+
+###########################################################################
 
 url = 'https://www4.stat.ncsu.edu/~boos/var.select/diabetes.tab.txt'
 df2 = pd.read_csv(url, header=0, sep='\t')
-print(df2)
+# print(df2.head())
+# print(df2)
+#
+# plt.scatter(df2.BMI, df2.Y)
+# plt.show()
+#
+# plt.scatter(df2.S5, df2.Y)
+# plt.show()
+#
+# plt.scatter(df2.S6, df.Y)
+# plt.show()
 
-plt.scatter(df2.BMI, df2.Y)
-plt.show()
+###########################################################################
 
-plt.scatter(df2.S5, df2.Y)
-plt.show()
-
-plt.scatter(df2.S6, df.Y)
-plt.show()
-
-# Создание матричной диаграммы рассеяния
-sns.pairplot(df, vars=diabetes.feature_names, hue='target')
-
-# Отображение графика
-plt.show()
-
+# # Создание матричной диаграммы рассеяния
+# sns.pairplot(df, vars=diabetes.feature_names, hue='target')
+#
+# # Отображение графика
+# plt.show()
 
 #Вывод: судя по данным о пациентах с диабетом, индекс массы тела больше 25 является серьезным фактором диабета
 
+###########################################################################
 
 # # Вывод первых 5 строк датасета
 # print(df.head())
@@ -51,3 +61,64 @@ plt.show()
 # print(diabetes.target.shape)
 # print(diabetes.DESCR)
 # print(diabetes.feature_names)
+
+###########################################################################
+
+# reg = MLPRegressor(hidden_layer_sizes=(1, ), activation='identity', solver='lbfgs')
+# x = np.array(df2.BMI)
+# y = np.array(df2.Y)
+# reg.fit(x.reshape(-1, 1), y)
+# yy = reg.predict(x.reshape(-1, 1))
+# plt.scatter(df2.BMI, df2.Y)
+# plt.plot(x, yy, color="green")
+# plt.show()
+
+###########################################################################
+
+#MLP Regressor
+# reg = MLPRegressor(hidden_layer_sizes=(1, ), alpha=1e-4, activation='identity', solver='lbfgs', verbose=10, tol=1e-4,
+#                    learning_rate_init=.1, random_state=1)
+# reg2 = MLPRegressor(hidden_layer_sizes=(1, ), alpha=1e-4, activation='identity', solver='lbfgs', verbose=10, tol=1e-4,
+#                    learning_rate_init=.1, random_state=1)
+# x = np.array(df2.BMI)
+# y = np.array(df2.Y)
+#
+# reg.fit(x.reshape(-1, 1), y)
+# yy = reg.predict(x.reshape(-1, 1))
+#
+# reg2.fit(y.reshape(-1, 1), x)
+# xx = reg2.predict(y.reshape(-1, 1))
+#
+# plt.scatter(df2.BMI, df2.Y)
+#
+# plt.plot(x, yy, color="red")
+# plt.plot(xx, y, color="green")
+#
+# plt.show()
+
+###########################################################################
+
+# r2_BMI = r2_score(df2.Y, yy)
+# print(r2_BMI)
+
+###########################################################################
+
+# corr_matrix = df2.corr()
+#
+# print(corr_matrix)
+#
+# sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+# plt.show()
+
+###########################################################################
+
+reg3 = MLPRegressor(hidden_layer_sizes=(1, ), alpha=1e-4, activation='identity', solver='lbfgs', verbose=10, tol=1e-4,
+                   learning_rate_init=.1, random_state=1)
+x = df2.drop(["Y"], axis=1)
+reg3.fit(x, df2.Y)
+yy = reg3.predict(x)
+r2_mlp10 = r2_score(df2.Y, yy)
+print(r2_mlp10)
+
+plt.scatter(df2.Y, yy)
+plt.show()
---
+++ keras_tensorflow.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+
+url = 'https://www4.stat.ncsu.edu/~boos/var.select/diabetes.tab.txt'
+df = pd.read_csv(url, header=0, sep='\t')
+
+model_bmi = Sequential()
+model_bmi.add(Dense(1, input_dim=1, activation='linear'))
+model_bmi.compile(loss='mean_squared_error', optimizer='adam')
+
+model_bmi.fit(df.BMI, df.Y, epochs=1000)
+
+keras_bmi_pridict = model_bmi.predict(df.BMI)
+
+plt.scatter(df.BMI, df.Y)
+plt.plot(df.BMI, keras_bmi_pridict, color='red')
+plt.show()
+
+history = model_bmi.fit(df.BMI, df.Y, epochs=2000)
+
+print(type(history))
+
+print(history)
+
+print(type(history.history))
+
+print(history.history.keys())
+
+print(history.history['loss'])
+
+plt.plot(history.history['loss'])
+# plt.ylim('Функция потерь')
+# plt.xlim('Эпохи')
+plt.show()
+
+X = np.array(df.BMI).reshape(-1, 1)
+X_train, X_test, y_train, y_test = train_test_split(X, df.Y, test_size=0.2, random_state=42)
+
+history = model_bmi.fit(X_train, y_train, epochs=1000, validation_data=(X_test, y_test))
+
+print(history.history.keys())
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+# plt.ylim('Функция потерь')
+# plt.xlim('Эпохи')
+plt.show()
+
+weights = model_bmi.get_weights()[0]
+bias = model_bmi.get_weights()[-1]
+
+print(weights, bias)
+
+print(model_bmi.weights)
+
+xx = np.array([20, 40])
+yy = bias[0] + xx * weights[0][0]
+
+keras_bmi_pridict = model_bmi.predict(X)
+plt.scatter(X, df.Y)
+plt.plot(X, keras_bmi_pridict, color="red")
+plt.plot(xx, yy, color="green")
+plt.show()
